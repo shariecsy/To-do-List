20a8592f329c7d9577b8275d4cc032b2001a415a
shariecsy <328561659@qq.com>
2016-07-08 14:06:39 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var app = express();
+
+//
 app.use(express.static('toDoList'));
+
+app.post('/save', function(req, res) {
+    console.log('save');
+    res.end('ok');
+})
 app.listen(1337);
